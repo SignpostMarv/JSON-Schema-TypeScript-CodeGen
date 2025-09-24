e56e47beac2bdef335e1588c4dfd982fe12dda28
SignpostMarv <me@signpostmarv.name>
2025-09-24 16:36:14 +0000

    relocating test

--- tests/src/JSONSchema/Object/ObjectUnspecified.spec.ts
+++ tests/src/JSONSchema/Object/ObjectUnspecified.spec.ts
@@ -26,6 +26,7 @@ import ts_assert from '@signpostmarv/ts-assert';
 
 import type {
 	object_properties_mode,
+	object_schema,
 	object_type,
 	object_TypeLiteralNode,
 } from '../../../../src/JSONSchema/Object.ts';
@@ -52,6 +53,153 @@ import {
 } from '../../../../src/guarded.ts';
 
 void describe('ObjectUnspecified', () => {
+	void describe('::generate_default_schema_definition()', () => {
+		type DataSet<
+			PropertiesMode extends (
+				object_properties_mode
+			) = (
+				object_properties_mode
+			),
+		> = [
+			PropertiesMode,
+		];
+
+		const full_schema_properties: Readonly<object_schema<
+			'both'
+		>['properties']> = Object.freeze({
+			$defs: {
+				type: 'object',
+				additionalProperties: {
+					type: 'object',
+				},
+			},
+			type: {type: 'string', const: 'object'},
+			required: {
+				type: 'array',
+				minItems: 1,
+				items: {
+					type: 'string',
+					minLength: 1,
+				},
+			},
+			properties: {
+				type: 'object',
+				minProperties: 1,
+				additionalProperties: {
+					type: 'object',
+				},
+			},
+			patternProperties: {
+				type: 'object',
+				minProperties: 1,
+				additionalProperties: {
+					type: 'object',
+				},
+			},
+		});
+
+		const expected_require_sets: Record<
+			object_properties_mode,
+			object_schema<
+				object_properties_mode
+			>['required']
+		> = {
+			neither: ['type'],
+			both: ['type', 'properties', 'patternProperties'],
+			properties: ['type', 'properties'],
+			pattern: ['type', 'patternProperties'],
+		};
+
+		const property_sets: Readonly<Record<
+			object_properties_mode,
+			object_schema<
+				object_properties_mode
+			>['properties']
+		>> = {
+			neither: {
+				type: full_schema_properties.type,
+				$defs: full_schema_properties.$defs,
+				required: full_schema_properties.required,
+			},
+			both: {
+				type: full_schema_properties.type,
+				$defs: full_schema_properties.$defs,
+				required: full_schema_properties.required,
+				properties: full_schema_properties.properties,
+				patternProperties: (
+					full_schema_properties.patternProperties
+				),
+			},
+			properties: {
+				type: full_schema_properties.type,
+				$defs: full_schema_properties.$defs,
+				required: full_schema_properties.required,
+				properties: full_schema_properties.properties,
+			},
+			pattern: {
+				type: full_schema_properties.type,
+				$defs: full_schema_properties.$defs,
+				required: full_schema_properties.required,
+				patternProperties: (
+					full_schema_properties.patternProperties
+				),
+			},
+		};
+
+		const data_sets: DataSet[] = [];
+
+		const properties_modes: [
+			object_properties_mode,
+			object_properties_mode,
+			object_properties_mode,
+			object_properties_mode,
+		] = [
+			'neither',
+			'both',
+			'properties',
+			'pattern',
+		];
+
+		for (const properties_mode of properties_modes) {
+			data_sets.push([
+				properties_mode,
+			]);
+		}
+
+		data_sets.forEach(([
+			properties_mode,
+		], i) => {
+			void it(
+				`behaves with data_sets[${
+					i
+				}] (object_schema<${
+					properties_mode
+				}>)`,
+				() => {
+					const schema = ObjectUnspecified
+						.generate_default_schema_definition({
+							properties_mode,
+						});
+
+					const required = expected_require_sets[
+						properties_mode
+					];
+
+					const properties = property_sets[properties_mode];
+
+					const expected_schema = {
+						type: 'object',
+						required,
+						additionalProperties: false,
+						properties,
+					};
+
+					assert.deepEqual(schema, expected_schema);
+				},
+			);
+		})
+	});
+
 	type DataSet<
 		T extends {[key: string]: unknown} = {[key: string]: unknown},
 		DefsMode extends 'optional' = 'optional',
