0fbf3a3f988085d5fe46dc26e6a6e9d41e0f1d1b
SignpostMarv <me@signpostmarv.name>
2025-09-25 19:13:50 +0000

    removing unused guards

--- src/guarded.ts
+++ src/guarded.ts
@@ -40,53 +40,6 @@ type PositiveIntegerOrZero<T extends number> = (
 	| PositiveInteger<T>
 );
 
-type Float<T extends number> = (
-	& (
-		`${T}` extends `${string}.${string}`
-			? T
-			: never
-	)
-	& {
-		_guard_Float: never,
-	}
-);
-
-export function NonZero<T extends number>(value: Exclude<T, 0>): NonZero
-{
-	return value as number as NonZero;
-}
-
-export function Positive<
-	T extends number,
->(value: `${T}` extends `-${string}` ? never : Exclude<T, 0>): Positive<T>
-{
-	return NonZero(value) as Positive<T>;
-}
-
-export function Integer<
-	T extends number,
->(
-	value: `${T}` extends `${string}.${string}` ? never : T,
-): Integer<T> {
-	return value as Integer<T>;
-}
-
-export function Float<
-	T extends number,
->(
-	value: (
-		`${T}` extends `${string}.${string}`
-			? T
-			: (
-				`${T}` extends `${0}`
-					? T
-					: never
-			)
-	),
-): Float<T> {
-	return value as Float<T>;
-}
-
 export function PositiveInteger<
 	T extends number,
 >(
